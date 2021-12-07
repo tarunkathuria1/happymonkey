88f96d276943f615f83845fe6fedfba881953dcf
tarunkathuria1 <72028064+tarunkathuria1@users.noreply.github.com>
2021-12-07 18:50:46 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,83 +3,126 @@ var monkey , monkey_running
 var banana ,bananaImage, obstacle, obstacleImage
 var FoodGroup, obstacleGroup
 var score
-var survivalTime;
 
-var canvas, backgroundImage;
-
-var gameState = 0;
-var playerCount;
-var allPlayers;
-var distance = 0;
-var database;
-
-var form, player, game;
-var car1,car2,car3,car4,cars;
 function preload(){
   
   
-  monkey_running = loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
   
   bananaImage = loadImage("banana.png");
-  obstacleImage = loadImage("obstacle.png");
+  obstaceImage = loadImage("obstacle.png");
  
 }
 
 
 
 function setup() {
-    createCanvas(400, 400);
-    database = firebase.database();
-    game = new Game();
-    game.getState();
-    game.start();
+  // createCanvas(600, 600);
+  
 
-    ground=createSprite(400,350,900,10);
-    ground.velocityX=-4;
-    ground.x=ground.width/2;
-    console.log(ground.x);
-    
-    
+
+  var survivalTime=0;
+  
+  //creating monkey
+   monkey=createSprite(80,315,20,20);
+   monkey.addAnimation("moving", monkey_running);
+  // monkey.addImage(bananaImage)
+   monkey.scale=0.1
+  
+  ground = createSprite(400,350,900,10);
+  ground.velocityX=-4;
+  ground.x=ground.width/2;
+  console.log(ground.x)
+
+  FoodGroup = new Group();
+  obstaclesGroup = new Group();
+
+  score = 0;
  
   
- 
 }
 
 
 function draw() {
-background("white");
   
-if(playerCount === 4){
-  game.update(1);
-}
-if(gameState === 1){
-  clear();
-  game.play();
-}
-if(gameState===2){
-  game.end();
-}
+  background(255);
   
-if (ground.x < 0){
-  ground.x = ground.width/2;
-}   
-
+    
+  if(ground.x<0) {
+    ground.x=ground.width/2;
+  }
   
   
+   
+    if(keyDown("space") ) {
+      monkey.velocityY = -12;
+    }
+    monkey.velocityY = monkey.velocityY + 0.8;
+  
+    monkey.collide(ground);   
+    spawnFood();
+    spawnObstacles();
  
-  
-drawSprites();
-   stroke("white");
+  drawSprites();
+  stroke("white");
   textSize(20);
   fill("white");
-  text("Score: "+score,500,50);
+  text("Score: "+ score, 500,50);        
+  
+  
+    if(obstaclesGroup.isTouching(monkey)){
+        ground.velocityX = 0;
+        monkey_running.velocityY = 0;
+        obstaclesGroup.setVelocityXEach(0);
+        FoodGroup.setVelocityXEach(0);
+        obstaclesGroup.setLifetimeEach(-1);
+        FoodGroup.setLifetimeEach(-1);
+    
+    
+    }
   
   stroke("black");
   textSize(20);
   fill("black");
-  survivalTime=Math.ceil(frameCount/frameRate());
-  text("Survival Time; "+survivalTime,100,50);
+  survivalTime=Math.ceil(frameCount/frameRate()) 
+  text("Survival Time: "+ survivalTime, 100,50);
 }
 
 
 
+function spawnFood() {
+  //write code here to spawn the Food
+  if (frameCount % 80 === 0) {
+    banana = createSprite(600,250,40,10);
+    banana.y = random(120,200);    
+    banana.velocityX = -5;
+    
+     //assign lifetime to the variable
+    banana.lifetime = 300;
+    monkey.depth = banana.depth + 1;
+    
+    //add image of banana
+     banana.addImage(bananaImage);
+     banana.scale=0.05;
+    
+    //add each banana to the group
+    FoodGroup.add(banana);
+  }
+}
+
+function spawnObstacles() {
+  if(frameCount % 300 === 0) {
+    obstacle = createSprite(800,320,10,40);
+    obstacle.velocityX = -6;
+    
+    //add image to the obstacle 
+    obstacle.addImage(obstaceImage);
+    obstacle.scale=0.15;
+    
+    //lifetime to the obstacle     
+    obstacle.lifetime = 300;
+    
+    //add each obstacle to the group
+    obstaclesGroup.add(obstacle);
+  }
+}
